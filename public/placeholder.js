aa2087a3f855029c00728fcffaedf33c82042b5d
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2023-10-11 22:34:31 -0400

    weird

--- public/placeholder.js
+++ public/placeholder.js
@@ -1,4 +1,3 @@
-//create pushes table 
 var pages = ['page1', 'page2', 'page3'];
 var currentPageIndex = -1;
 console.log(currentPageIndex + " top of js");
@@ -6,18 +5,17 @@ var username = " "; //document.querySelector('.namereg'); same as user.displayNa
 var password = " ";
 var email = " ";
 var signtype = "";
-let loggedInPerson = false;
+
 console.log(currentPageIndex);
 var showNextPage = function() {
     currentPageIndex = (currentPageIndex + 1) % pages.length;
     console.log("page index = ", currentPageIndex)
     var template = document.getElementById(pages[currentPageIndex]).innerHTML;
     //do stuff to template here
-    display.innerHTML = template;
     if (currentPageIndex == 2) {
-        renderTournament();
         console.log("populate? full html means to use AJAX and pull stuff from DB about it");
     }
+    display.innerHTML = template;
 };
 var showPreviousPage = function() {
     currentPageIndex = (currentPageIndex - 1) % pages.length;
@@ -28,8 +26,8 @@ var showPreviousPage = function() {
     display.innerHTML = template;
 
 };
-
-/*
+var google_provider = new firebase.auth.GoogleAuthProvider();
+google_provider.setCustomParameters({ prompt: 'select_account' });
 firebase.auth().onAuthStateChanged(user => {
     if (!!user) {
         //currentPageIndex = 0;
@@ -43,8 +41,6 @@ firebase.auth().onAuthStateChanged(user => {
                     console.log("error");
                 });
             console.log(user.displayName, " after catch");
-        } else {
-            username = user.displayName;
         }
         showNextPage();
         console.log(user + " : after sign in") ///////////////////////////////////////////////////////////////////
@@ -59,21 +55,20 @@ firebase.auth().onAuthStateChanged(user => {
         renderLogin();
     }
 });
-*/
-/*let renderTournament = () => {
-
-};*/
 let renderLogin = () => {
     console.log("entered");
     console.log($("#logbutton")[0]);
     $("#logbutton").on("click", () => {
         firebase.auth().signInWithRedirect(google_provider);
-        //const tryF = firebase.auth().currentUser;
-        //console.log(tryF.displayName, " test");
+        const tryF = firebase.auth().currentUser;
+        console.log(tryF.displayName, " test");
         //showNextPage();
     });
     $("#registerbutton").on("click", () => {
         const modalReg = $("#modalReg")[0];
+        //$("#fnameR").val("dadadadada"); //sets the value
+        //const jj = $("#fnameR").val(); //will get the value
+        //console.log(jj);
         $("#fnameR").val("");
         $("#pwdR").val("");
         $("#email").val("");
@@ -90,9 +85,7 @@ let renderLogin = () => {
                 "Email": email
             };
             modalReg.close();
-            firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredentials) => {
-                var pp = userCredentials.user;
-                console.log(pp.displayName, " dd");
+            firebase.auth().createUserWithEmailAndPassword(email, password).then((user) => {
                 user.updateProfile({ displayName: username.toString() }).then(function() {
                         console.log(user.displayName, " inside 2");
                     })
@@ -114,10 +107,9 @@ let startApp = (user) => {
     if (signtype === "free" && user.displayName == null) {
         user.updateProfile({ displayName: username });
     }
+    $("#namereg").text(user.displayName);
     console.log("hi");
     console.log(user.displayName + ": udispo");
-    console.log(username + ": uddddd");
-    $("#namereg").text(username);
     $("#logoutbutton").on("click", () => {
         firebase.auth().signOut();
     });
@@ -127,23 +119,26 @@ let startApp = (user) => {
         $("#gname").val("");
         $("#nump").val("");
         $("#gpwd").val("");
+        $("#pbox").val("");
         modalCreate.showModal();
         $("#closeCreate").click(() => {
             const tourneyName = $("#tname").val();
             const gamename = $("#gname").val();
             const numplayers = $("#nump").val();
             const gamepwd = $("#gpwd").val();
+            const publicbox = $("#pbox").val();
             const ownersArry = [user];
             const ownerDisplayName = user.displayName;
             const ownerEmail = user.email;
             const playersArry = [];
             const matches = [];
-            const joinableLink = ("https://cisc472-tourney.web.app/", "Tournament/"); // ,tourneyName, "/", gamename, "/", ownerDisplayName);
+            const joinableLink = ("https://cisc472-tourney.web.app/", tourneyName, "/", gamename, "/", ownerDisplayName);
             let obj = { //add more if needed for bracket tourney
                 "TournamentName": tourneyName,
                 "GameName": gamename,
                 "NumPlayers": numplayers,
                 "GamePassword": gamepwd,
+                "PublicGame": publicbox,
                 "Owners": ownersArry,
                 "Players": playersArry,
                 "Games": matches,
@@ -154,83 +149,12 @@ let startApp = (user) => {
             //then ajax stuff into game on firebase
             const modalCreate = $("#modalCreate")[0];
             modalCreate.close();
-            //showNextPage();
+            showNextPage();
         });
-        //console.log("redering");
-        //firebase.database().ref("/surveys").on("value", ss => {});
 
     });
-    console.log("redering");
 
 
 };
-let routeToPage = (parts, user) => {
-    //addLogout();
-    console.log(parts.length);
-    if (parts.length < 3) {
-        showNextPage();
-        startApp(user);
-    } else {
-        if (parts[1] == "tournament" && parts[2].length > 1) {
-            //renderSurvey(parts[2]);
-            console.log("teehee");
-        } else {
-            alert("Tournament Doesnt Exist / Invalid Link");
-            showNextPage();
-            startApp(user);
-        }
-    }
-}
-var google_provider = new firebase.auth.GoogleAuthProvider();
-google_provider.setCustomParameters({ prompt: 'select_account' });
-document.addEventListener('DOMContentLoaded', function() {
-    let pn = document.location.pathname;
-    let URLparts = pn.split("/");
 
-    firebase.auth().onAuthStateChanged(user => {
-        if (!!user) {
-            //currentPageIndex = 0;
-            loggedInPerson = user;
-            console.log(signtype, ": sty ", username, " : uname");
-            if (signtype === "free" && user.displayName == null) {
-                console.log("enter HERE!");
-                user.updateProfile({ displayName: username.toString() }).then(function() {
-                        console.log(user.displayName, " inside 1");
-                    })
-                    .catch(function(error) {
-                        console.log("error");
-                    });
-                console.log(user.displayName, " after catch");
-            } else {
-                username = user.displayName;
-            }
-            //showNextPage();
-            console.log(user + " : after sign in") ///////////////////////////////////////////////////////////////////
-            console.log("after login");
-            routeToPage(URLparts, user);
-            //startApp(user);
-        } else {
-            if (currentPageIndex == 1) {
-                showPreviousPage();
-            }
-            loggedInPerson = false;
-            console.log(user + " : initial null user") //////////////////////////////////////////////////////////////
-            console.log("First AUTH");
-            renderLogin();
-        }
-    });
-    //Expecting /tourney/:uidhere
-    /*
-    firebase.auth().onAuthStateChanged(user => {
-      if (!!user){
-        loggedInPerson = user;
-        routeToPage(URLparts);
-      } else {
-        loggedInPerson = false;
-        $("#logout-wrap").remove();
-        renderLogin();
-      }
-    });*/
-    console.log("SPLIT!");
-});
 showNextPage();
